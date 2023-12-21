e28893c06cbcfa7cd7bfd4fa9a3814dd54585622
Eric Pantke <eric.pantke@encoway.de>
2023-12-21 11:03:24 +0100

    better navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,25 +1,36 @@
-import { Component } from '@angular/core';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 import {RouterLink} from "@angular/router";
-import {MatListModule} from "@angular/material/list";
 
 @Component({
   selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
   standalone: true,
   imports: [
     MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule,
     MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
     RouterLink,
-    MatListModule
-  ],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  ]
 })
 export class NavbarComponent {
+  private breakpointObserver = inject(BreakpointObserver);
 
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
