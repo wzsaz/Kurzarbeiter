e8eaa2263f0404c308f21675c55cb18c09b84639
Eric Pantke <eric.pantke@encoway.de>
2024-01-10 09:59:01 +0100

    Update token fetching method in AuthServer
    The method used for fetching tokens in AuthServer.py has been updated. Now it utilizes the os module for getting the absolute path to the getToken.ps1 script, ensuring that the script can be located consistently regardless of the current working directory.

--- auth_proxy/AuthServer.py
+++ auth_proxy/AuthServer.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 import subprocess
 import logging
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -16,7 +17,8 @@ logging.getLogger('').addHandler(console)
 def get_token():
   app.logger.info("Fetching new token...")
   try:
-    result = subprocess.run(['powershell.exe', './auth_proxy/getToken.ps1'], capture_output=True, text=True)
+    script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'getToken.ps1')
+    result = subprocess.run(['powershell.exe', script_path], capture_output=True, text=True)
     formattedresult = result.stdout.strip()
     app.logger.info('Result: %s', formattedresult)  # Log the result
     return formattedresult
