78d54c824b576ffccc4cffefa60b2ab6b1cc7d68
Eric Pantke <eric.pantke@encoway.de>
2024-01-17 10:20:59 +0100

    ...

--- src/app/employeeview/employeeview.component.ts
+++ src/app/employeeview/employeeview.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {EmployeeResponseDTO, EmployeeUIState} from "../types";
+import {EmployeeUIState} from "../types";
 import {EmployeeService} from "../service/employee.service";
 import {NgForOf, NgIf} from "@angular/common";
 import {MatButtonModule} from "@angular/material/button";
@@ -7,6 +7,8 @@ import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
 import {MatChipsModule} from "@angular/material/chips";
 import {FilterComponent} from "../filter/filter.component";
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-employeeview',
@@ -28,11 +30,29 @@ export class EmployeeviewComponent implements OnInit {
 
   @Output() edit = new EventEmitter<EmployeeUIState>();
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService, public dialog: MatDialog) { } // Inject MatDialog
 
   ngOnInit(): void {
     this.employeeService.getEmployees().subscribe(employees => {
       this.employees = employees.map(employee => ({ ...employee, showDetails: false, pictureUrl: 'path_to_picture' }));
+    });
+  }
+
+  openDeleteDialog(employee: EmployeeUIState): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        message: `Are you sure you want to delete ${employee.firstName} ${employee.lastName}?`,
+        buttonText: {
+          ok: 'Yes',
+          cancel: 'No'
+        }
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        this.onDelete(employee);
+      }
     });
   }
 
@@ -48,5 +68,11 @@ export class EmployeeviewComponent implements OnInit {
     // Implement your filter logic here.
     // This might set a filter on your employees array or fetch a new list from your service.
     console.log("Filter applied: " + filterValue);
+  }
+
+  onDelete(employee: EmployeeUIState): void {
+    this.employeeService.deleteEmployee(employee.id).subscribe(() => {
+      this.employees = this.employees.filter(e => e.id !== employee.id);
+    });
   }
 }
