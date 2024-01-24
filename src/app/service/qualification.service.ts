9883bc7c78a9690bf9a4a69f2f57f58c64e7a8c5
epantke <ericpantke@gmail.com>
2024-01-24 16:24:19 +0100

    Improve deleteQualification function in service
    The deleteQualification function in the qualification service has been optimized. It now checks first if there are employees with a particular qualification before deletion. If employees have the qualification, it gets removed from their skill set first before proceeding with the deletion. This ensures accurate and comprehensive data management.

--- src/app/service/qualification.service.ts
+++ src/app/service/qualification.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {forkJoin, Observable, of, switchMap} from 'rxjs';
-import {QualificationDTO} from '../types';
+import {EmployeeRequestDTO, EmployeeResponseDTO, QualificationDTO} from '../types';
 import {BaseService} from "./base.service";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -56,10 +57,39 @@ export class QualificationService extends BaseService {
     );
   }
 
-  deleteQualification(id: number) {
+  deleteQualification(qualificationId: number): Observable<EmployeeResponseDTO[] | null> {
     return this.setAuthHeader().pipe(
       switchMap(headers => {
-        return headers ? this.handleRequest(this.http.delete(`${this.apiUrl}/${id}`, {headers})) : of(null);
+        if (!headers) {
+          return of(null);
+        }
+
+        // First, get the list of employees that have this qualification
+        return this.http.get<EmployeeResponseDTO[]>(`${this.apiUrl}/${qualificationId}/employees`, {headers}).pipe(
+          switchMap(employees => {
+            if (employees.length > 0) {
+              // If there are employees with this qualification, delete the qualification from their skill set
+              const updateRequests = employees.map(employee => {
+                const updatedEmployee = {...employee};
+                updatedEmployee.skillSet = updatedEmployee.skillSet.filter(skill => skill.id !== qualificationId);
+                return this.http.put<EmployeeRequestDTO>(`http://127.0.0.1:8089/employees/${updatedEmployee.id}`, updatedEmployee, {headers});
+              });
+              return forkJoin(updateRequests).pipe(
+                switchMap(() => {
+                  // After updating all employees, proceed with the deletion of the qualification
+                  return this.http.delete(`${this.apiUrl}/${qualificationId}`, {headers}).pipe(
+                    map(() => null) // Return null to indicate that the qualification was deleted
+                  );
+                })
+              );
+            } else {
+              // If there are no employees with this qualification, proceed with the deletion
+              return this.http.delete(`${this.apiUrl}/${qualificationId}`, {headers}).pipe(
+                map(() => null) // Return null to indicate that the qualification was deleted
+              );
+            }
+          })
+        );
       })
     );
   }
