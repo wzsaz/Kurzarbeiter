ee210734d127f17d02db1055a9149453a584b41a
Wagdy Wassim Abo Aed <wagdywassim.aboaed@encoway.de>
2024-01-26 14:42:44 +0100

    Added autocomplete for non-functional filter

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,12 +1,23 @@
-import {Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import {NgIf} from "@angular/common";
+import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
 import {Employee} from "../types";
 import {EmployeesComponent} from "../employees/employees.component";
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from "@angular/material/autocomplete";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {map} from "rxjs/operators";
+import {Observable, startWith} from "rxjs";
+import {
+  MatAccordion,
+  MatExpansionPanel,
+  MatExpansionPanelDescription,
+  MatExpansionPanelTitle
+} from "@angular/material/expansion";
 
 @Component({
   selector: 'app-filter',
@@ -18,7 +29,18 @@ import {EmployeesComponent} from "../employees/employees.component";
     MatButtonModule,
     MatIconModule,
     NgIf,
-    EmployeesComponent
+    EmployeesComponent,
+    NgForOf,
+    MatAutocomplete,
+    MatOption,
+    AsyncPipe,
+    ReactiveFormsModule,
+    MatAutocompleteTrigger,
+    MatCheckbox,
+    MatExpansionPanel,
+    MatExpansionPanelTitle,
+    MatAccordion,
+    MatExpansionPanelDescription
   ],
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css']
@@ -27,9 +49,71 @@ export class FilterComponent implements OnChanges {
   @Input() employeesToFilter: Employee[] = [];
   filteredEmployees: Employee[] = [];
 
-  // Automatically exectue when the input changes
+  firstNameFilter = new FormControl();
+  lastNameFilter = new FormControl();
+  cityFilter = new FormControl();
+  phoneFilter = new FormControl();
+
+  firstNameOptions: string[] = [];
+  lastNameOptions: string[] = [];
+  cityOptions: string[] = [];
+  phoneOptions: string[] = [];
+
+  filteredFirstNameOptions: Observable<string[]>;
+  filteredLastNameOptions: Observable<string[]>;
+  filteredCityOptions: Observable<string[]>;
+  filteredPhoneOptions: Observable<string[]>;
+
+  constructor() {
+    this.filteredFirstNameOptions = this.firstNameFilter.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value, this.firstNameOptions))
+    );
+
+    this.filteredLastNameOptions = this.lastNameFilter.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value, this.lastNameOptions))
+    );
+
+    this.filteredCityOptions = this.cityFilter.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value, this.cityOptions))
+    );
+
+    this.filteredPhoneOptions = this.phoneFilter.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value, this.phoneOptions))
+    );
+  }
+
   ngOnChanges() {
-    // TODO: true
-    this.filteredEmployees = this.employeesToFilter.filter(employee => true);
+    this.firstNameOptions = [...new Set(this.employeesToFilter.map(employee => employee.firstName))];
+    this.lastNameOptions = [...new Set(this.employeesToFilter.map(employee => employee.lastName))];
+    this.cityOptions = [...new Set(this.employeesToFilter.map(employee => employee.city))];
+    this.phoneOptions = [...new Set(this.employeesToFilter.map(employee => employee.phone))];
+
+    this.filterEmployees();
+  }
+
+  filterEmployees() {
+    const firstName = this.firstNameFilter.value;
+    const lastName = this.lastNameFilter.value;
+    const city = this.cityFilter.value;
+    const phone = this.phoneFilter.value;
+
+    this.filteredEmployees = this.employeesToFilter.filter(employee => {
+      const firstNameMatches = !firstName || employee.firstName.includes(firstName);
+      const lastNameMatches = !lastName || employee.lastName.includes(lastName);
+      const cityMatches = !city || employee.city.includes(city);
+      const phoneMatches = !phone || employee.phone.includes(phone);
+
+      return firstNameMatches && lastNameMatches && cityMatches && phoneMatches;
+    });
+  }
+
+  private _filter(value: string, options: string[]): string[] {
+    const filterValue = value.toLowerCase();
+
+    return options.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
