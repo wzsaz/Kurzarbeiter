a418982788c1aaa609d79931c42385d372fc8ed6
epantke <ericpantke@gmail.com>
2024-01-27 00:46:10 +0100

    Activate AuthguardService and switch Docker image
    This commit reverts the Docker container image to 'employee-management-service:1.1.1' and comments out the 'employee-management-service_without_keycloak:1.1' image. On another hand, the AuthguardService has been activated for the routing paths in 'app.routes.ts'. Future updates might include CanDeactivateGuardService for specified components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import {EditorComponent} from "./editor/editor.component";
 import {AuthguardService} from "./service/authguard.service";
 import {QualificationsComponent} from "./qualifications/qualifications.component";
 import {HomeComponent} from "./home/home.component";
+import {CanDeactivateGuardService} from "./service/can-deactivate-guard.service";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'view', component: ViewComponent, /*canActivate: [AuthguardService]*/},
-  {path: 'editor', component: EditorComponent, /*canActivate: [AuthguardService]*/},
-  {path: 'editor/:id', component: EditorComponent, /*canActivate: [AuthguardService]*/},
-  {path: 'qualifications', component: QualificationsComponent, /*canActivate: [AuthguardService]*/},
+  {path: 'view', component: ViewComponent, canActivate: [AuthguardService]},
+  {path: 'editor', component: EditorComponent, canActivate: [AuthguardService], /*canDeactivate: [CanDeactivateGuardService]*/},
+  {path: 'editor/:id', component: EditorComponent, canActivate: [AuthguardService], /*canDeactivate: [CanDeactivateGuardService]*/},
+  {path: 'qualifications', component: QualificationsComponent, canActivate: [AuthguardService]},
 ];
