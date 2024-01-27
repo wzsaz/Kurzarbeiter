df3bb5891a33aea3c3238bccf1a12ac6c7d5ba88
epantke <ericpantke@gmail.com>
2024-01-27 02:32:23 +0100

    Replace custom cookie consent dialog with ngx-cookieconsent
    Replaced the custom implementation of the cookie consent dialog with a widely used third-party library ngx-cookieconsent for cleaner and more efficient cookie policy management. This change also includes deletion of custom cookie dialog code and configuration of the new cookie consent in the application config. Make sure to replace placeholder URLs with the actual policy URLs in the future.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,14 +5,55 @@ import {routes} from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+import {NgcCookieConsentConfig, provideNgcCookieConsent} from "ngx-cookieconsent";
+
+const cookieConfig: NgcCookieConsentConfig = {
+  cookie: {
+    domain: 'localhost' // Replace with your domain
+  },
+  palette: {
+    popup: {
+      background: '#000'
+    },
+    button: {
+      background: '#f1d600'
+    }
+  },
+  theme: 'edgeless',
+  type: 'opt-out',
+  layout: 'my-custom-layout',
+  layouts: {
+    "my-custom-layout": '{{messagelink}}{{compliance}}'
+  },
+  elements: {
+    messagelink: `
+      <span id="cookieconsent:desc" class="cc-message">{{message}}
+        <a aria-label="learn more about cookies" tabindex="0" class="cc-link" href="{{cookiePolicyHref}}" target="_blank">{{cookiePolicyLink}}</a>.
+        We also have a <a aria-label="learn more about our privacy policy" tabindex="1" class="cc-link" href="{{privacyPolicyHref}}" target="_blank">{{privacyPolicyLink}}</a> and
+        <a aria-label="learn more about our terms of service" tabindex="2" class="cc-link" href="{{tosHref}}" target="_blank">{{tosLink}}</a>.
+        Enjoy your stay!</span>
+    `,
+  },
+  content: {
+    message: 'This website uses cookies. By continuing, you agree to be bombarded with our delicious cookies. ',
+    cookiePolicyLink: 'Cookie Policy',
+    cookiePolicyHref: 'https://your-cookie-policy-url',
+    privacyPolicyLink: 'Privacy Policy',
+    privacyPolicyHref: 'https://your-privacy-policy-url',
+    tosLink: 'Terms of Service',
+    tosHref: 'https://your-terms-service-url'
+  }
+};
 
 const keycloakProvider = [
   {provide: APP_INITIALIZER, useFactory: initializeKeycloak, multi: true, deps: [KeycloakService]},
 ]
+
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(KeycloakAngularModule),
     keycloakProvider,
+    provideNgcCookieConsent(cookieConfig),
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations()
@@ -33,6 +74,3 @@ export function initializeKeycloak(keycloak: KeycloakService): () => Promise<boo
       }
     })
 }
-
-
-
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {ConfirmationSnackbarComponent} from "../confirm-dialog/confirm-snackbar.component";
-import {MatDialog} from "@angular/material/dialog";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,25 +7,5 @@ import {MatDialog} from "@angular/material/dialog";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  constructor(private dialog: MatDialog) { }
-
-  ngOnInit() {
-    if (!sessionStorage.getItem('dialogShown')) {
-      this.openDialog();
-    }
-  }
-
-  openDialog() {
-    sessionStorage.setItem('dialogShown', 'true');
-
-    const dialogRef = this.dialog.open(ConfirmationSnackbarComponent, {
-      width: '500px',
-      disableClose: true
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
+export class HomeComponent {
 }
